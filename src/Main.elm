2a88ed5c33419ab9ac7eb219d9ddf9ee763b7f2a
michael-ilcsy <michael_ilcsy_git@yahoo.co.jp>
2021-01-22 23:55:22 +0900

    feat: とりあえずランダムな3桁の数字を表示するところまで

--- src/Main.elm
+++ src/Main.elm
@@ -1,20 +1,50 @@
 module Main exposing (..)
 
+import Array exposing (Array)
 import Browser
-import Html exposing (Html, text, div, h1, img)
-import Html.Attributes exposing (src)
+import Html exposing (Html, div, h1, text)
+import Random
+import Random.Array
+
 
 
 ---- MODEL ----
 
 
 type alias Model =
-    {}
+    { questionAnswer : ThreeNumber
+    }
+
+
+type ThreeNumber
+    = ThreeNumber Int Int Int
+
+
+getFromIntArray : Int -> Array Int -> Int
+getFromIntArray index array =
+    -- 本当は例外処理か絶対数字があることを保証する型を書かないといけない
+    Maybe.withDefault -1 (Array.get index array)
+
+
+arrayToThreeNumber : Array Int -> ThreeNumber
+arrayToThreeNumber array =
+    ThreeNumber (getFromIntArray 0 array) (getFromIntArray 1 array) (getFromIntArray 2 array)
+
+
+randomThreeNumber : Random.Generator ThreeNumber
+randomThreeNumber =
+    Random.Array.shuffle (Array.fromList (List.range 0 9))
+        |> Random.map arrayToThreeNumber
+
+
+generateRandomThreeNumber : Cmd Msg
+generateRandomThreeNumber =
+    Random.generate GenerateRandomThreeNumber randomThreeNumber
 
 
 init : ( Model, Cmd Msg )
 init =
-    ( {}, Cmd.none )
+    ( { questionAnswer = ThreeNumber 0 0 0 }, generateRandomThreeNumber )
 
 
 
@@ -22,12 +52,14 @@ init =
 
 
 type Msg
-    = NoOp
+    = GenerateRandomThreeNumber ThreeNumber
 
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
-    ( model, Cmd.none )
+    case msg of
+        GenerateRandomThreeNumber threeNumber ->
+            ( { model | questionAnswer = threeNumber }, Cmd.none )
 
 
 
@@ -37,9 +69,13 @@ update msg model =
 view : Model -> Html Msg
 view model =
     div []
-        [ img [ src "/logo.svg" ] []
-        , h1 [] [ text "Your Elm App is working!" ]
+        [ viewThreeNumber model.questionAnswer
         ]
+
+
+viewThreeNumber : ThreeNumber -> Html msg
+viewThreeNumber (ThreeNumber one two three) =
+    h1 [] [ text <| String.fromInt one ++ String.fromInt two ++ String.fromInt three ]
 
 
 
